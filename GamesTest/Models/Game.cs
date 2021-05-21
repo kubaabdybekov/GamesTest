560cdc82183608a734dbded29c0e5cfe03a80263
kubaabdybekov <abdybekovkuba@gmail.com>
2021-05-22 02:23:47 +0500

    Добавьте файлы проекта.

---
+++ GamesTest/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesTest.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Developer { get; set; }
+        public string Genre { get; set; }
+        
+    }
+}
